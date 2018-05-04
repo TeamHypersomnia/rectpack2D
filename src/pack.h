843481df7810dba9fbf54de0df71cf87edfed213
Patryk Czachurski <patryk.czachurski@gmail.com>
2018-05-04 19:49:06 +0200

    put symbols under namespace

--- src/pack.h
+++ src/pack.h
@@ -4,297 +4,304 @@
 
 #include "3rdparty/rectpack2D/src/pack_structs.h"
 
-struct node {
-	rect_ltrb rc;
-	node(rect_ltrb rc = rect_ltrb()) : rc(rc) {}
+namespace rectpack {
+	struct node {
+		rect_ltrb rc;
+		node(rect_ltrb rc = rect_ltrb()) : rc(rc) {}
 
-private:
-	struct pnode {
-		node* pn = nullptr;
-		bool has_trash = true;
+	private:
+		struct pnode {
+			node* pn = nullptr;
+			bool has_trash = true;
 
-		bool has_child() const {
-			return pn != nullptr && !has_trash;
+			bool has_child() const {
+				return pn != nullptr && !has_trash;
+			}
+
+			void set(const rect_ltrb& r) {
+				if (pn == nullptr) { 
+					pn = new node(r); 
+				}
+				else {
+					pn->rc = r;
+					pn->id = false;
+				}
+
+				has_trash = false;
+			}
+		};
+
+		pnode child[2];
+		bool id = false;
+
+		void delcheck() {
+			if(child[0].pn) { child[0].has_trash = true; child[0].pn->delcheck(); }
+			if(child[1].pn) { child[1].has_trash = true; child[1].pn->delcheck(); }
 		}
 
-		void set(const rect_ltrb& r) {
-			if (pn == nullptr) { 
-				pn = new node(r); 
+		node* split(rect_xywhf& img, const bool allow_flip) {
+			const auto iw = img.flipped ? img.h : img.w;
+			const auto ih = img.flipped ? img.w : img.h;
+
+			if (rc.w() - iw > rc.h() - ih) {
+				child[0].set({ rc.l, rc.t, rc.l+iw, rc.b });
+				child[1].set({ rc.l+iw, rc.t, rc.r, rc.b });
 			}
 			else {
-				pn->rc = r;
-				pn->id = false;
+				child[0].set({ rc.l, rc.t, rc.r, rc.t + ih });
+				child[1].set({ rc.l, rc.t + ih, rc.r, rc.b });
 			}
 
-			has_trash = false;
-		}
-	};
-
-	pnode c[2];
-	bool id = false;
-
-	void delcheck() {
-		if(c[0].pn) { c[0].has_trash = true; c[0].pn->delcheck(); }
-		if(c[1].pn) { c[1].has_trash = true; c[1].pn->delcheck(); }
-	}
-
-	node* split(rect_xywhf& img, const bool allow_flip) {
-		const auto iw = img.flipped ? img.h : img.w;
-		const auto ih = img.flipped ? img.w : img.h;
-
-		if (rc.w() - iw > rc.h() - ih) {
-			c[0].set({ rc.l, rc.t, rc.l+iw, rc.b });
-			c[1].set({ rc.l+iw, rc.t, rc.r, rc.b });
-		}
-		else {
-			c[0].set({ rc.l, rc.t, rc.r, rc.t + ih });
-			c[1].set({ rc.l, rc.t + ih, rc.r, rc.b });
+			return child[0].pn->insert(img, allow_flip);
 		}
 
-		return c[0].pn->insert(img, allow_flip);
-	}
+	public:
+		node* insert(rect_xywhf& img, const bool allow_flip) {
+			if (child[0].has_child()) {
+				if (const auto inserted_left = child[0].pn->insert(img, allow_flip)) {
+					return inserted_left;
+				}
 
-public:
-	node* insert(rect_xywhf& img, const bool allow_flip) {
-		if (c[0].has_child()) {
-			if (const auto inserted_left = c[0].pn->insert(img, allow_flip)) {
-				return inserted_left;
+				/* Insert to the right otherwise */
+				return child[1].pn->insert(img,allow_flip);
 			}
 
-			/* Insert to the right otherwise */
-			return c[1].pn->insert(img,allow_flip);
-		}
+			if (id) {
+				return nullptr;
+			}
 
-		if (id) {
-			return nullptr;
-		}
-
-		switch (img.fits(rect_xywh(rc), allow_flip)) {
-			case rect_wh_fitting::TOO_BIG: 
+			switch (img.fits(rect_xywh(rc), allow_flip)) {
+				case rect_wh_fitting::TOO_BIG: 
 				return nullptr;
 
-			case rect_wh_fitting::FITS_INSIDE:
+				case rect_wh_fitting::FITS_INSIDE:
 				img.flipped = false; 
 
 				return split(img, allow_flip); 
 
-			case rect_wh_fitting::FITS_INSIDE_BUT_FLIPPED: 	
+				case rect_wh_fitting::FITS_INSIDE_BUT_FLIPPED: 	
 				img.flipped = true; 
 
 				return split(img, allow_flip);
 
-			case rect_wh_fitting::FITS_EXACTLY:
+				case rect_wh_fitting::FITS_EXACTLY:
 				id = true; 
 				img.flipped = false; 
 
 				return this;
 
-			case rect_wh_fitting::FITS_EXACTLY_BUT_FLIPPED: 
+				case rect_wh_fitting::FITS_EXACTLY_BUT_FLIPPED: 
 				id = true; 
 				img.flipped = true;  
 
 				return this;
+			}
 		}
-	}
 
-	void reset(const rect_wh& r) {
-		id = false;
-		rc = rect_ltrb(0, 0, r.w, r.h);
-		delcheck();
-	}
+		void reset(const rect_wh& r) {
+			id = false;
+			rc = rect_ltrb(0, 0, r.w, r.h);
+			delcheck();
+		}
 
-	~node() {
-		if(c[0].pn) delete c[0].pn;
-		if(c[1].pn) delete c[1].pn;
-	}
-};
+		~node() {
+			if (child[0].pn) {
+				delete child[0].pn;
+			}
 
-template <class F, class G, class... Comparators>
-rect_wh pack_rectangles(
-	const std::vector<rect_xywhf*>& input, 
-	const int max_bin_side, 
-	const bool allow_flip, 
-	F push_successful,
-	G push_unsuccessful,
-	const int discard_step,
-	Comparators... comparators
-) {
-	node root;
-	const int n = input.size();
-
-	constexpr auto funcs = sizeof...(Comparators);
-
-	bool (*cmpf[funcs])(rect_xywhf*, rect_xywhf*) = {
-		comparators...
+			if (child[1].pn) {
+				delete child[1].pn;
+			}
+		}
 	};
 
-	thread_local std::vector<rect_xywhf*> order[funcs];
+	template <class F, class G, class... Comparators>
+	rect_wh pack_rectangles(
+		const std::vector<rect_xywhf*>& input, 
+		const int max_bin_side, 
+		const bool allow_flip, 
+		F push_successful,
+		G push_unsuccessful,
+		const int discard_step,
+		Comparators... comparators
+	) {
+		node root;
+		const int n = input.size();
 
-	for (unsigned f = 0; f < funcs; ++f) { 
-		order[f] = input;
-		std::sort(order[f].begin(), order[f].end(), cmpf[f]);
-	}
+		constexpr auto funcs = sizeof...(Comparators);
 
-	rect_wh min_bin = rect_wh(max_bin_side, max_bin_side);
+		bool (*cmpf[funcs])(rect_xywhf*, rect_xywhf*) = {
+			comparators...
+		};
 
-	std::optional<int> min_func;
+		thread_local std::vector<rect_xywhf*> order[funcs];
 
-	int	best_func = 0;
-	int best_area = 0;
+		for (unsigned f = 0; f < funcs; ++f) { 
+			order[f] = input;
+			std::sort(order[f].begin(), order[f].end(), cmpf[f]);
+		}
 
-	int current_area = 0;
+		rect_wh min_bin = rect_wh(max_bin_side, max_bin_side);
 
-	bool fail = false;
+		std::optional<int> min_func;
 
-	for (unsigned f = 0; f < funcs; ++f) {
-		const auto& v = order[f];
+		int	best_func = 0;
+		int best_area = 0;
 
-		int step = min_bin.w / 2;
-		root.reset(min_bin);
+		int current_area = 0;
 
-		while (true) {
-			if (root.rc.w() > min_bin.w) {
-				/* 
-					If we are now going to attempt packing into a bin
-					that is bigger than the current minimum, abort.
-				*/
+		bool fail = false;
 
-				if (min_func) {
+		for (unsigned f = 0; f < funcs; ++f) {
+			const auto& v = order[f];
+
+			int step = min_bin.w / 2;
+			root.reset(min_bin);
+
+			while (true) {
+				if (root.rc.w() > min_bin.w) {
+					/* 
+						If we are now going to attempt packing into a bin
+						that is bigger than the current minimum, abort.
+					*/
+
+					if (min_func) {
+						break;
+					}
+
+					current_area = 0;
+
+					root.reset(min_bin);
+
+					for (int i = 0; i < n; ++i) {
+						if (root.insert(*v[i],allow_flip)) {
+							current_area += v[i]->area();
+						}
+					}
+
+					fail = true;
 					break;
 				}
 
-				current_area = 0;
-
-				root.reset(min_bin);
-
-				for (int i = 0; i < n; ++i) {
-					if (root.insert(*v[i],allow_flip)) {
-						current_area += v[i]->area();
+				const bool all_inserted = [&]() {
+					for (int i = 0; i < n; ++i) {
+						if (!root.insert(*v[i], allow_flip)) {
+							return false;
+						}
 					}
-				}
 
-				fail = true;
-				break;
-			}
+					return true;
+				}();
 
-			const bool all_inserted = [&]() {
-				for (int i = 0; i < n; ++i) {
-					if (!root.insert(*v[i], allow_flip)) {
-						return false;
+				if (all_inserted) {
+					if (step <= discard_step) {
+						break;
 					}
+
+					/* Attempt was successful. Try with a smaller bin. */
+					root.reset({ root.rc.w() - step, root.rc.h() - step });
+				}
+				else {
+					/* Attempt ended in failure. Try with a bigger bin. */
+					root.reset({ root.rc.w() + step, root.rc.h() + step });
 				}
 
-				return true;
-			}();
+				step /= 2;
 
-			if (all_inserted) {
-				if (step <= discard_step) {
-					break;
+				if (!step) {
+					step = 1;
 				}
-
-				/* Attempt was successful. Try with a smaller bin. */
-				root.reset({ root.rc.w() - step, root.rc.h() - step });
-			}
-			else {
-				/* Attempt ended in failure. Try with a bigger bin. */
-				root.reset({ root.rc.w() + step, root.rc.h() + step });
 			}
 
-			step /= 2;
-
-			if (!step) {
-				step = 1;
+			if (!fail && (min_bin.area() >= root.rc.area())) {
+				min_bin = rect_wh(root.rc);
+				min_func = f;
 			}
+			else if (fail && (current_area > best_area)) {
+				best_area = current_area;
+				best_func = f;
+			}
+
+			fail = false;
 		}
 
-		if (!fail && (min_bin.area() >= root.rc.area())) {
-			min_bin = rect_wh(root.rc);
-			min_func = f;
-		}
-		else if (fail && (current_area > best_area)) {
-			best_area = current_area;
-			best_func = f;
-		}
+		int clip_x = 0;
+		int clip_y = 0;
 
-		fail = false;
-	}
+		{
+			auto& v = order[min_func ? *min_func : best_func];
 
-	int clip_x = 0;
-	int clip_y = 0;
+			root.reset(min_bin);
 
-	{
-		auto& v = order[min_func ? *min_func : best_func];
+			for (int i = 0; i < n; ++i) {
+				if(auto ret = root.insert(*v[i],allow_flip)) {
+					v[i]->x = ret->rc.l;
+					v[i]->y = ret->rc.t;
 
-		root.reset(min_bin);
+					if(v[i]->flipped) {
+						v[i]->flipped = false;
+						v[i]->flip();
+					}
 
-		for (int i = 0; i < n; ++i) {
-			if(auto ret = root.insert(*v[i],allow_flip)) {
-				v[i]->x = ret->rc.l;
-				v[i]->y = ret->rc.t;
+					clip_x = std::max(clip_x, ret->rc.r);
+					clip_y = std::max(clip_y, ret->rc.b); 
 
-				if(v[i]->flipped) {
+					push_successful(v[i]);
+				}
+				else {
+					push_unsuccessful(v[i]);
+
 					v[i]->flipped = false;
-					v[i]->flip();
 				}
-
-				clip_x = std::max(clip_x, ret->rc.r);
-				clip_y = std::max(clip_y, ret->rc.b); 
-
-				push_successful(v[i]);
-			}
-			else {
-				push_unsuccessful(v[i]);
-
-				v[i]->flipped = false;
 			}
 		}
+
+		return rect_wh(clip_x, clip_y);
 	}
 
-	return rect_wh(clip_x, clip_y);
-}
+	template <class F, class G>
+	rect_wh pack_rectangles(
+		const std::vector<rect_xywhf*>& input, 
+		const int max_bin_side, 
+		const bool allow_flip, 
+		F push_successful,
+		G push_unsuccessful,
+		const int discard_step
+	) {
+		auto area = [](rect_xywhf* a, rect_xywhf* b) {
+			return a->area() > b->area();
+		};
 
-template <class F, class G>
-rect_wh pack_rectangles(
-	const std::vector<rect_xywhf*>& input, 
-	const int max_bin_side, 
-	const bool allow_flip, 
-	F push_successful,
-	G push_unsuccessful,
-	const int discard_step
-) {
-	auto area = [](rect_xywhf* a, rect_xywhf* b) {
-		return a->area() > b->area();
-	};
+		auto perimeter = [](rect_xywhf* a, rect_xywhf* b) {
+			return a->perimeter() > b->perimeter();
+		};
 
-	auto perimeter = [](rect_xywhf* a, rect_xywhf* b) {
-		return a->perimeter() > b->perimeter();
-	};
+		auto max_side = [](rect_xywhf* a, rect_xywhf* b) {
+			return std::max(a->w, a->h) > std::max(b->w, b->h);
+		};
 
-	auto max_side = [](rect_xywhf* a, rect_xywhf* b) {
-		return std::max(a->w, a->h) > std::max(b->w, b->h);
-	};
+		auto max_width = [](rect_xywhf* a, rect_xywhf* b) {
+			return a->w > b->w;
+		};
 
-	auto max_width = [](rect_xywhf* a, rect_xywhf* b) {
-		return a->w > b->w;
-	};
+		auto max_height = [](rect_xywhf* a, rect_xywhf* b) {
+			return a->h > b->h;
+		};
 
-	auto max_height = [](rect_xywhf* a, rect_xywhf* b) {
-		return a->h > b->h;
-	};
+		return _rect2D(
+			input,
 
-	return _rect2D(
-		input,
+			max_bin_side,
+			allow_flip,
+			push_successful,
+			push_unsuccessful,
+			discard_step,
 
-		max_bin_side,
-		allow_flip,
-		push_successful,
-		push_unsuccessful,
-		discard_step,
-
-		area,
-		perimeter,
-		max_side,
-		max_width,
-		max_height
-	);
+			area,
+			perimeter,
+			max_side,
+			max_width,
+			max_height
+		);
+	}
 }
--- src/pack_structs.h
+++ src/pack_structs.h
@@ -2,144 +2,146 @@
 #include <cstring>
 #include <algorithm>
 
-struct rect_ltrb;
-struct rect_xywh;
+namespace rectpack {
+	struct rect_ltrb;
+	struct rect_xywh;
 
-enum class rect_wh_fitting {
-	TOO_BIG,
-	FITS_INSIDE,
-	FITS_INSIDE_BUT_FLIPPED,
-	FITS_EXACTLY,
-	FITS_EXACTLY_BUT_FLIPPED
-};
+	enum class rect_wh_fitting {
+		TOO_BIG,
+		FITS_INSIDE,
+		FITS_INSIDE_BUT_FLIPPED,
+		FITS_EXACTLY,
+		FITS_EXACTLY_BUT_FLIPPED
+	};
 
-struct rect_wh {
-	rect_wh(const rect_ltrb&);
-	rect_wh(const rect_xywh&);
-	rect_wh(int w = 0, int h = 0);
-	int w, h, area(), perimeter();
+	struct rect_wh {
+		rect_wh(const rect_ltrb&);
+		rect_wh(const rect_xywh&);
+		rect_wh(int w = 0, int h = 0);
+		int w, h, area(), perimeter();
 
-	rect_wh_fitting fits(const rect_wh& r, const bool allow_flip) const {
-		if (w == r.w && h == r.h) {
-			return rect_wh_fitting::FITS_EXACTLY;
-		}
-
-		if (allow_flip) {
-			if (h == r.w && w == r.h) {
-				return rect_wh_fitting::FITS_EXACTLY_BUT_FLIPPED;	
+		rect_wh_fitting fits(const rect_wh& r, const bool allow_flip) const {
+			if (w == r.w && h == r.h) {
+				return rect_wh_fitting::FITS_EXACTLY;
 			}
-		}
 
-		if (w <= r.w && h <= r.h) {
-			return rect_wh_fitting::FITS_INSIDE;
-		}
-
-		if (allow_flip) {
-			if (h <= r.w && w <= r.h) {
-				return rect_wh_fitting::FITS_INSIDE_BUT_FLIPPED;
+			if (allow_flip) {
+				if (h == r.w && w == r.h) {
+					return rect_wh_fitting::FITS_EXACTLY_BUT_FLIPPED;	
+				}
 			}
-		}
 
-		return rect_wh_fitting::TOO_BIG;
+			if (w <= r.w && h <= r.h) {
+				return rect_wh_fitting::FITS_INSIDE;
+			}
+
+			if (allow_flip) {
+				if (h <= r.w && w <= r.h) {
+					return rect_wh_fitting::FITS_INSIDE_BUT_FLIPPED;
+				}
+			}
+
+			return rect_wh_fitting::TOO_BIG;
+		}
+	};
+
+	struct rect_ltrb {
+		rect_ltrb();
+		rect_ltrb(int left, int top, int right, int bottom);
+		int l, t, r, b, w() const, h() const, area() const, perimeter() const;
+		void w(int), h(int);
+	};
+
+	struct rect_xywh : public rect_wh {
+		rect_xywh();
+		rect_xywh(const rect_ltrb&);
+		rect_xywh(int x, int y, int width, int height);
+		operator rect_ltrb();
+
+		int x, y, r() const, b() const;
+		void r(int), b(int);
+	};
+
+	struct rect_xywhf : public rect_xywh {
+		rect_xywhf(const rect_ltrb&);
+		rect_xywhf(int x, int y, int width, int height);
+		rect_xywhf();
+		void flip();
+		bool flipped;
+	};
+
+	rect_wh::rect_wh(const rect_ltrb& rr) : w(rr.w()), h(rr.h()) {} 
+	rect_wh::rect_wh(const rect_xywh& rr) : w(rr.w), h(rr.h) {} 
+	rect_wh::rect_wh(int w, int h) : w(w), h(h) {}
+
+	rect_ltrb::rect_ltrb() : l(0), t(0), r(0), b(0) {}
+	rect_ltrb::rect_ltrb(int l, int t, int r, int b) : l(l), t(t), r(r), b(b) {}
+
+	int rect_ltrb::w() const {
+		return r-l;
 	}
-};
 
-struct rect_ltrb {
-	rect_ltrb();
-	rect_ltrb(int left, int top, int right, int bottom);
-	int l, t, r, b, w() const, h() const, area() const, perimeter() const;
-	void w(int), h(int);
-};
+	int rect_ltrb::h() const {
+		return b-t;
+	}
 
-struct rect_xywh : public rect_wh {
-	rect_xywh();
-	rect_xywh(const rect_ltrb&);
-	rect_xywh(int x, int y, int width, int height);
-	operator rect_ltrb();
+	int rect_ltrb::area() const {
+		return w()*h();
+	}
 
-	int x, y, r() const, b() const;
-	void r(int), b(int);
-};
+	int rect_ltrb::perimeter() const {
+		return 2*w() + 2*h();
+	}
 
-struct rect_xywhf : public rect_xywh {
-	rect_xywhf(const rect_ltrb&);
-	rect_xywhf(int x, int y, int width, int height);
-	rect_xywhf();
-	void flip();
-	bool flipped;
-};
+	void rect_ltrb::w(int ww) {
+		r = l+ww;
+	}
 
-rect_wh::rect_wh(const rect_ltrb& rr) : w(rr.w()), h(rr.h()) {} 
-rect_wh::rect_wh(const rect_xywh& rr) : w(rr.w), h(rr.h) {} 
-rect_wh::rect_wh(int w, int h) : w(w), h(h) {}
+	void rect_ltrb::h(int hh) {
+		b = t+hh;
+	}
 
-rect_ltrb::rect_ltrb() : l(0), t(0), r(0), b(0) {}
-rect_ltrb::rect_ltrb(int l, int t, int r, int b) : l(l), t(t), r(r), b(b) {}
+	rect_xywh::rect_xywh() : x(0), y(0) {}
+	rect_xywh::rect_xywh(const rect_ltrb& rc) : x(rc.l), y(rc.t) { b(rc.b); r(rc.r); }
+	rect_xywh::rect_xywh(int x, int y, int w, int h) : rect_wh(w, h), x(x), y(y) {}
 
-int rect_ltrb::w() const {
-	return r-l;
-}
+	rect_xywh::operator rect_ltrb() {
+		rect_ltrb rr(x, y, 0, 0);
+		rr.w(w); rr.h(h);
+		return rr;
+	}
 
-int rect_ltrb::h() const {
-	return b-t;
-}
+	int rect_xywh::r() const {
+		return x+w;
+	};
 
-int rect_ltrb::area() const {
-	return w()*h();
-}
+	int rect_xywh::b() const {
+		return y+h;
+	}
 
-int rect_ltrb::perimeter() const {
-	return 2*w() + 2*h();
-}
+	void rect_xywh::r(int right) {
+		w = right-x;
+	}
 
-void rect_ltrb::w(int ww) {
-	r = l+ww;
-}
+	void rect_xywh::b(int bottom) {
+		h = bottom-y;
+	}
 
-void rect_ltrb::h(int hh) {
-	b = t+hh;
-}
+	int rect_wh::area() {
+		return w*h;
+	}
 
-rect_xywh::rect_xywh() : x(0), y(0) {}
-rect_xywh::rect_xywh(const rect_ltrb& rc) : x(rc.l), y(rc.t) { b(rc.b); r(rc.r); }
-rect_xywh::rect_xywh(int x, int y, int w, int h) : rect_wh(w, h), x(x), y(y) {}
-
-rect_xywh::operator rect_ltrb() {
-	rect_ltrb rr(x, y, 0, 0);
-	rr.w(w); rr.h(h);
-	return rr;
-}
-
-int rect_xywh::r() const {
-	return x+w;
-};
-
-int rect_xywh::b() const {
-	return y+h;
-}
-
-void rect_xywh::r(int right) {
-	w = right-x;
-}
-
-void rect_xywh::b(int bottom) {
-	h = bottom-y;
-}
-
-int rect_wh::area() {
-	return w*h;
-}
-
-int rect_wh::perimeter() {
-	return 2*w + 2*h; 
-}
+	int rect_wh::perimeter() {
+		return 2*w + 2*h; 
+	}
 
 
-rect_xywhf::rect_xywhf(const rect_ltrb& rr) : rect_xywh(rr), flipped(false) {}
-rect_xywhf::rect_xywhf(int x, int y, int width, int height) : rect_xywh(x, y, width, height), flipped(false) {}
-rect_xywhf::rect_xywhf() : flipped(false) {}
+	rect_xywhf::rect_xywhf(const rect_ltrb& rr) : rect_xywh(rr), flipped(false) {}
+	rect_xywhf::rect_xywhf(int x, int y, int width, int height) : rect_xywh(x, y, width, height), flipped(false) {}
+	rect_xywhf::rect_xywhf() : flipped(false) {}
 
-void rect_xywhf::flip() { 
-	flipped = !flipped;
-	std::swap(w, h);
+	void rect_xywhf::flip() { 
+		flipped = !flipped;
+		std::swap(w, h);
+	}
 }
