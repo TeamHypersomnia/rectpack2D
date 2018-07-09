1dd9e3d5d990d62a25ee540b332584a69246b552
Patryk Czachurski <patryk.czachurski@gmail.com>
2018-07-09 22:56:53 +0200

    Fix script

--- cmake/print_bt_if_core_found.sh
+++ cmake/print_bt_if_core_found.sh
@@ -3,7 +3,7 @@ CORE_PATH=$1
 
 if [ -f "$CORE_PATH" ]; then
 	echo "Core found at $CORE_PATH." > $OUTPUT_TERM
-	gdb -ex="bt" -ex="q" build/current/Hypersomnia $CORE_PATH
+	gdb -ex="bt" -ex="q" build/current/rectpack2D $CORE_PATH
 else
 	echo "Core not found at $CORE_PATH."
 fi
