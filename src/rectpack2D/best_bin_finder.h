999fe19ba46984e0a6361e352acebc721ee44927
Petr Šácha <petrik.sacha@gmail.com>
2025-11-13 09:51:11 +0100

    remove redundant best_callback

--- src/rectpack2D/best_bin_finder.h
+++ src/rectpack2D/best_bin_finder.h
@@ -263,7 +263,6 @@ namespace rectpack2D {
 					if (*total_inserted > best_total_inserted) {
 						best_order_has_value = true;
 						best_total_inserted = *total_inserted;
-						best_callback();
 					}
 				}
 			}
